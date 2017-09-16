0bd7ddb78df1ef1896b223b77f018c072c3aa28d
Lucas Schmitz <lucas.schmitz123@hotmail.com>
2017-09-16 15:07:35 -0300

    acrescentada a pergunta sobre como voce esta

--- ExemploGit/Program.cs
+++ ExemploGit/Program.cs
@@ -12,6 +12,7 @@ namespace ExemploGit
         {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
